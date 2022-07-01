b3457d0e9786724e0aadb1da6a78d4ebbed51199
Tomas Blaser <tomas.blaser@gmail.com>
2022-07-01 08:30:22 -0300

    rne

--- FoodYouApp/App.js
+++ FoodYouApp/App.js
@@ -1,15 +1,16 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text, View, Button,Linking} from 'react-native';
 import Navigation from './Navigation/Navigation.js';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
 
 
 export default function App() {
-  return (
-       <Navigation/>
-  );
+  return <SafeAreaProvider>
+    <Navigation/>
+  </SafeAreaProvider>;
+       
+  
 }
 
 
