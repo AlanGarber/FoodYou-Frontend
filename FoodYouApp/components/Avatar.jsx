330bc0c85fd091cfc331fbe2ae7e8f99f8a7acc1
Tomas Blaser <tomas.blaser@gmail.com>
2022-07-13 10:44:12 -0300

    hacer conexion con axios

---
+++ FoodYouApp/components/Avatar.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { Avatar } from "@rneui/base";
+
+const FotoUser = () => {
+  return (
+    <Avatar
+      activeOpacity={0.2}
+      avatarStyle={{}}
+      containerStyle={{ backgroundColor: "#41AC92" }}
+      icon={{}}
+      iconStyle={{}}
+      imageProps={{}}
+      overlayContainerStyle={{}}
+      placeholderStyle={{}}
+      rounded
+      size="large"
+      title="FY"
+      titleStyle={{color:"white"}}
+    />
+  );
+}
+
+export default FotoUser;
--- FoodYouApp/screens/ProfileScreen.jsx
+++ FoodYouApp/screens/ProfileScreen.jsx
@@ -1,17 +1,29 @@
 import * as React from 'react';
 import {View} from 'react-native';
+import { Input } from "@rneui/base";
 import InputApellido from '../components/InputApellido';
 import InputNombre from '../components/InputNombre';
 import BotonEnviar from '../components/BotonEnviar';
 import CheckBoxes from '../components/CheckBoxes';
+import FotoUser from '../components/Avatar';
 
 export default function ProfileScreen({navigation}) {
     return(
-        <View style={{flex: 1, alignItems: 'center', justifyContent:'center'}}>
-                <InputNombre/>
-                <InputApellido/>
-                <CheckBoxes/>
+        <View style={{flex: 1, alignItems: 'center', justifyContent:'center', backgroundColor:'#41AC92'}}>
+            <View style={{alignItems: 'center', justifyContent:'center', width:'75%', backgroundColor:'white', borderTopLeftRadius: 20, borderTopRightRadius: 20, borderBottomLeftRadius: 20, borderBottomRightRadius: 20,}}>
+                <FotoUser/>
+                <Input
+                    disabledInputStyle={{ background: "#ddd" }}
+                    placeholder="Name"
+                    placeholderTextColor="black"
+                />
+                <Input
+                    disabledInputStyle={{ background: "#ddd" }}
+                    placeholder="Last Name"
+                    placeholderTextColor="black"
+                />
                 <BotonEnviar/>
+            </View>
         </View>
     )
 }
